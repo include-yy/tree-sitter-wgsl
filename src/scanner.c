5613f604d650094847caa4901b8d2f348fe7debb
include-yy <969041171@qq.com>
2024-02-14 23:09:25 +0800

    use rust's impl of scanner.c for nested block comment
    src/scanner.c: use the newest impl from tree-sitter-rust
    
    According to spec of WGSL, block comment could be nested,
    thus we need a external scanner to deal with it.

--- src/scanner.c
+++ src/scanner.c
@@ -1,73 +1,100 @@
 #include <tree_sitter/parser.h>
 #include <wctype.h>
 
-enum TokenType {
-	BLOCK_COMMENT
-};
+// from tree-sitter-rust/src/scanner.c
+// https://github.com/tree-sitter/tree-sitter-rust/commit/17ce09d7b4b29bd4c37c5b3ad3b5d5c98a84b076
+// like rust, wgsl's block comment can be nested
 
-void *tree_sitter_wgsl_external_scanner_create() {
-	return NULL;
-}
-void tree_sitter_wgsl_external_scanner_destroy(void *p) {}
-void tree_sitter_wgsl_external_scanner_reset(void *p) {}
-unsigned int tree_sitter_wgsl_external_scanner_serialize(void *p, char *buffer) {
-	return 0;
-}
-void tree_sitter_wgsl_external_scanner_deserialize(void *p, const char *b, unsigned n) {}
+enum TokenType { BLOCK_COMMENT };
 
-static void advance(TSLexer *lexer) {
-	lexer->advance(lexer, false);
+void *
+tree_sitter_wgsl_external_scanner_create ()
+{
+    return NULL;
 }
 
-static bool at_eof(TSLexer *lexer) {
-	return lexer->eof(lexer);
+void
+tree_sitter_wgsl_external_scanner_destroy (void *p)
+{
 }
 
-// based on https://github.com/tree-sitter/tree-sitter-rust/blob/f7fb205c424b0962de59b26b931fe484e1262b35/src/scanner.c
-bool tree_sitter_wgsl_external_scanner_scan(
-	void *payload,
-	TSLexer *lexer,
-	const bool *valid_symbols
-	) {
-	while (iswspace(lexer->lookahead)) {
-		lexer->advance(lexer, true);
-	}
+void
+tree_sitter_wgsl_external_scanner_reset (void *p)
+{
+}
 
-	if (lexer->lookahead != '/') {
-		return false;
-	}
+unsigned int
+tree_sitter_wgsl_external_scanner_serialize (void *p, char *buffer)
+{
+    return 0;
+}
+
+void
+tree_sitter_wgsl_external_scanner_deserialize (void *p, const char *b, unsigned n)
+{
+}
+
+static void
+advance (TSLexer *lexer)
+{
+    lexer->advance(lexer, false);
+}
+
+bool
+tree_sitter_wgsl_external_scanner_scan (void *payload, TSLexer *lexer,
+					const bool *valid_symbols)
+{
+    while (iswspace(lexer->lookahead))
+	lexer->advance(lexer, true);
+
+    if (lexer->lookahead == '/')
+    {
+	advance(lexer);
+	if (lexer->lookahead != '*')
+	    return false;
 	advance(lexer);
 
-	if (lexer->lookahead != '*') {
-		return false;
-	}
-	advance(lexer);
-
-	unsigned int comment_depth = 1;
-	while (true) {
-		if (lexer->lookahead == '/') {
+	bool after_star = false;
+	unsigned int nesting_depth = 1;
+	for (;;)
+	{
+	    switch (lexer->lookahead)
+	    {
+		case '\0':
+		    return false;
+		case '*':
+		    advance(lexer);
+		    after_star = true;
+		    break;
+		case '/':
+		    if (after_star)
+		    {
 			advance(lexer);
-
-			if (lexer->lookahead == '*') {
-				advance(lexer);
-				comment_depth += 1;
+			after_star = false;
+			nesting_depth--;
+			if (nesting_depth == 0)
+			{
+			    lexer->result_symbol = BLOCK_COMMENT;
+			    return true;
 			}
-		} else if (lexer->lookahead == '*') {
+		    }
+		    else
+		    {
 			advance(lexer);
-
-			if (lexer->lookahead == '/') {
-				advance(lexer);
-				comment_depth -= 1;
-
-				if (comment_depth == 0) {
-					lexer->result_symbol = BLOCK_COMMENT;
-					return true;
-				}
+			after_star = false;
+			if (lexer->lookahead == '*')
+			{
+			    nesting_depth++;
+			    advance(lexer);
 			}
-		} else if (at_eof(lexer)) {
-			return false;
-		} else {
-			advance(lexer);
-		}
+		    }
+		    break;
+		default:
+		    advance(lexer);
+		    after_star = false;
+		    break;
+	    }
 	}
+    }
+    return false;
 }
